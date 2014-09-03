5ff001c2facbb0f4c282b4eec4fc2e13b08a8ddd
Alex Eastman <a-dog98@hotmail.com>
2014-09-03 12:58:08 -0500

    lol
    funny joke

---
+++ Turtle/Random_Code.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Random_Code
+{
+    public static void main(String[] args)
+    {
+        
+        Random generator = new Random();
+        System.out.println(generator.nextInt(6)+1);
+    }
+}
